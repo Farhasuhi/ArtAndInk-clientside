b613cf2a1bd111992471bd743fcf0d284904de60
Farhasuhi <git config --global user.email suhifarhaislam@gmail.com>
2023-06-12 20:07:07 +0600

    route will be indiviual for student,admin,intructor

--- src/Components/ArtsandCraftsTrainning/ArtsandCraftsTrainning.jsx
+++ src/Components/ArtsandCraftsTrainning/ArtsandCraftsTrainning.jsx
@@ -4,7 +4,7 @@ const ArtsandCraftsTrainning = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col w-full lg:flex-row gap-16">
-                <img src="https://images.unsplash.com/photo-1560421683-6856ea585c78?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXJ0cyUyMGFuZCUyMGNyYWZ0fGVufDB8fDB8fHww&w=1000&q=80" className="w-full lg:w-[600px] rounded-lg shadow-2xl" />
+                <img src="https://images.unsplash.com/photo-1560421683-6856ea585c78?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXJ0cyUyMGFuZCUyMGNyYWZ0fGVufDB8fDB8fHww&w=1000&q=80" className="w-full lg:w-[400px] rounded-lg shadow-2xl" />
                 <div>
                     <h1 className="text-5xl font-medium">International Based Learning!</h1>
                     <h1 className="text-5xl font-bold text-semibold text-purple-900">Arts and Crafts Training!</h1>
